1f6768b5844f0d4f529c2a89d337e289393e7593
Fontan <fontanvillacampa@gmail.com>
2024-07-08 20:41:13 +1000

    build

--- build.py
+++ build.py
@@ -7,51 +7,11 @@ import shutil
 def main():
       
     current_script_path = __file__
-    orbslam2_directory = os.path.dirname(os.path.abspath(current_script_path))
+    dso_directory = os.path.dirname(os.path.abspath(current_script_path))
                               
-    ## Compile DBoW2 
-    print("[ORB-SLAM2][build.py] Compiling DBoW2 (output disabled) ... ")   
-    source_folder = os.path.join(orbslam2_directory,"Thirdparty/DBoW2")      
-    build_folder = os.path.join(source_folder,"build")
-    bin_folder = os.path.join(source_folder,"bin") 
-    lib_folder = os.path.join(source_folder,"lib")
-    
-    if os.path.isdir(build_folder): 
-        shutil.rmtree(build_folder)  
-    if os.path.isdir(bin_folder): 
-        shutil.rmtree(bin_folder)  
-    if os.path.isdir(lib_folder): 
-        shutil.rmtree(lib_folder)          
-    if not os.path.isdir(build_folder): 
-        subprocess.run(["mkdir",build_folder])   
-   
-    subprocess.run(["cmake", "-G", "Ninja","-B", build_folder,"-S",os.path.join(source_folder),"-DCMAKE_PREFIX_PATH=" + source_folder,"-DCMAKE_INSTALL_PREFIX=" + source_folder], capture_output=True, text=True)   
-    subprocess.run(["cmake","--build", build_folder, "--config Release"], capture_output=True, text=True)
-    subprocess.run(["ninja","install","-C", build_folder], capture_output=True, text=True)
-    
-    ## Compile g2o 
-    print("[ORB-SLAM2][build.py] Compiling g2o (output disabled) ... ")   
-    source_folder = os.path.join(orbslam2_directory,"Thirdparty/g2o")      
-    build_folder = os.path.join(source_folder,"build")
-    bin_folder = os.path.join(source_folder,"bin") 
-    lib_folder = os.path.join(source_folder,"lib")
-    
-    if os.path.isdir(build_folder): 
-        shutil.rmtree(build_folder)  
-    if os.path.isdir(bin_folder): 
-        shutil.rmtree(bin_folder)  
-    if os.path.isdir(lib_folder): 
-        shutil.rmtree(lib_folder)          
-    if not os.path.isdir(build_folder): 
-        subprocess.run(["mkdir",build_folder])   
-   
-    subprocess.run(["cmake", "-G", "Ninja","-B", build_folder,"-S",os.path.join(source_folder),"-DCMAKE_PREFIX_PATH=" + source_folder,"-DCMAKE_INSTALL_PREFIX=" + source_folder], capture_output=True, text=True)   
-    subprocess.run(["cmake","--build", build_folder, "--config Release"], capture_output=True, text=True)
-    subprocess.run(["ninja","install","-C", build_folder], capture_output=True, text=True)
-    
-    ## Compile ORB-SLAM2      
-    print("[ORB-SLAM2][build.py] Compiling ORB-SLAM2 (output disabled) ... ")             
-    source_folder = orbslam2_directory
+    ## Compile dso   
+    print("[dso][build.py] Compiling DSO (output disabled) ... ")             
+    source_folder = dso_directory
     build_folder = os.path.join(source_folder,"build")
     bin_folder = os.path.join(source_folder,"bin") 
     lib_folder = os.path.join(source_folder,"lib")
